cb51337e3ff953bae4d249417292960c6769c43f
razany-maker <razan.yazid.m@gmail.com>
2026-05-27 06:03:07 +0300

    Delete Academic Advising Chatbot Website directory

--- Academic Advising Chatbot Website/src/app/App.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider } from 'react-router';
-import { router } from './routes';
-import '@xyflow/react/dist/style.css';
-
-export default function App() {
-  return <RouterProvider router={router} />;
-}
